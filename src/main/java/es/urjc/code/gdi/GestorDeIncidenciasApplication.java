e64419565c9d78e249d56d14e8317825d48db48c
miguelferdom <miguel.fer.dom@gmail.com>
2019-02-01 03:51:10 +0100

    Prueba de commit realizada desde el PC de casa manejado desde el PC del trabajo

---
+++ src/main/java/es/urjc/code/gdi/GestorDeIncidenciasApplication.java
@@ -0,0 +1,14 @@
+package es.urjc.code.gdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorDeIncidenciasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorDeIncidenciasApplication.class, args);
+	}
+
+}
+
