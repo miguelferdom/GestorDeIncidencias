ec638f4fe2e03717e64802dfb637272ae5a94205
miguelferdom <miguel.fer.dom@gmail.com>
2019-02-05 05:57:26 +0100

    Añado algunas plantillas mustache para intentar realcionar unas con otras. Funcionan algunas cosas pero hay que seguir trabajando a partir de bienvenida_template.mustache

--- src/main/java/es/urjc/code/gdi/GdiControllers.java
+++ src/main/java/es/urjc/code/gdi/GdiControllers.java
@@ -2,16 +2,52 @@ package es.urjc.code.gdi;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GdiControllers {
 	
 	@RequestMapping("/login")
-	public String cargalogin(Model model) {
+	public String cargaLogin(Model model) {
 
 		//model.addAttribute("name", "World");
 
 		return "login.html";
+	}
+	
+	@PostMapping("/bienvenida")
+	public String cargaBienvenida(Model model, @RequestParam String usuario, @RequestParam String password, @RequestParam String perfil) {
+		
+		model.addAttribute("user", usuario);
+		model.addAttribute("pass", password);
+		model.addAttribute("profile", perfil);
+		
+		return "bienvenida_template";
+	}
+	
+	@RequestMapping("/nuevaincidencia")
+	public String cargaNuevaIncidencia(Model model) {
+		return "nuevaincidencia_template";
+	}
+	
+	@PostMapping("/consultarincidencia")
+	//@RequestMapping("/consultarincidencia")
+	public String cargaConsultarIncidencia(Model model, @RequestParam String numincidencia) {
+		
+		//model.addAttribute("user", usuario);
+		//model.addAttribute("pass", password);
+		//model.addAttribute("profile", perfil);
+		model.addAttribute("issuenumber", numincidencia);
+				
+		return "consultarincidencia_template";
+	}
+	
+	//@PostMapping("/volverabienvenida")
+	@RequestMapping("/volverabienvenida")
+	public String cargaVolverABienvenida(Model model, @RequestParam String usuario, @RequestParam String password, @RequestParam String perfil) {
+		
+		return "bienvenida_template";
 	}
 }
