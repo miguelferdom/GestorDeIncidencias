2efbbd6fbbe1a5b15184976384f7400dc62d7b96
miguelferdom <miguel.fer.dom@gmail.com>
2019-02-10 07:01:35 +0100

    Creación de la clase Comentario.java
    Creo la clase Comentario.java con un constructor vacío, uno completo y
    los métodos get y set correspondientes para sus atributos. Reescribo el
    método toString() por si necesito visualizar el contenido en algún
    momento. Queda pendiente cambiar el tipo del atributo fecha de String a
    otro más adecuado, pero lo dejo así por el momento.

---
+++ src/main/java/es/urjc/code/gdi/Comentario.java
@@ -0,0 +1,69 @@
+package es.urjc.code.gdi;
+
+public class Comentario {
+
+	/* 
+	 * Declaro fecha como String para crear la clase rapidamente y poder hacer alguna prueba
+	 * Más adelante la cambiaré a un tipo más adecuado junto con sus métodos get y set y el
+	 * constructor completo
+	 */
+	private String fecha;
+	private String anotacion;
+	
+	/**
+	 * Constructor de la clase sin parámetros
+	 */
+	public Comentario () {}
+	
+	/**
+	 * Constructor completo de la clase
+	 * 
+	 * @param fecha fecha en la que se anota un Comentario en formato DD/MM/YYYY HH:SS
+	 * @param anotacion contenido del Comentario
+	 */
+	public Comentario (String fecha, String anotacion) {
+		setFecha (fecha);
+		setAnotacion ( anotacion);
+	}
+	
+	/**
+	 * Método set para establecer la fecha de un Comentario
+	 * 
+	 * @param fecha fecha en la que se anota un comentario en formato DD/MM/YYYY HH:SS
+	 */
+	public void setFecha (String fecha) {
+		this.fecha = fecha;
+	}
+	
+	/**
+	 * Método set para escribir el texto del Comentario
+	 * 
+	 * @param anotacion contenido del comentario
+	 */
+	public void setAnotacion (String anotacion) {
+		this.anotacion = anotacion;
+	}
+	
+	/**
+	 * Método get para devolver la Fecha de un Comentario
+	 * 
+	 * @return devuellve la Fecha de un Comentario
+	 */
+	public String getFecha () {
+		return this.fecha;
+	}
+	
+	/**
+	 * Método get para devolver el texto de un Comentario
+	 * 
+	 * @return devuelve el texto de un Comentario
+	 */
+	public String getAnotacion () {
+		return this.anotacion;
+	}
+	
+	@Override
+	public String toString() {
+		return ("[COMENTARIO][" + getFecha() + "]: " + getAnotacion());
+	}
+}
