bcba2bd76e64b9eccdba20635452045547f4fb0e
miguelferdom <miguel.fer.dom@gmail.com>
2019-02-10 09:47:41 +0100

    Modificacion de GdiControllers.java
    Añado un arrayList para cargar información de incidencias en memoria
    local para poder hacer pruebas. También modifico el controlador
    cargaLogin para que al hacer login muestre una plantilla a los usuarios
    de tipo usuario diferente a los de tipo tecnico y administrador.

--- src/main/java/es/urjc/code/gdi/GdiControllers.java
+++ src/main/java/es/urjc/code/gdi/GdiControllers.java
@@ -1,5 +1,7 @@
 package es.urjc.code.gdi;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,11 +11,25 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class GdiControllers {
 	
+	//creo una lista de incidencias para hacer de la aplicación con la información alojada en memoria
+	private ArrayList<Incidencia> incidencias = new ArrayList<>();
+	
+	// declaro el constructor vacio de la clase GdiControllers y lo uso para cargar información de incidencias en memoria
+	public GdiControllers () {
+		incidencias.add(new Incidencia("Alta", "Correo", "Aceptada", "solicitud de nuevo correo electronico", "Necesitamos un buzon de correo para el compañero user99"));
+		incidencias.add(new Incidencia("Alta", "Correo", "Abierta", "Baja de correo electronico", "solicitamos eliminar el buzon de correo del usuario user98 por causar baja en la empresa"));
+		incidencias.add(new Incidencia("Alta", "SW ventas", "Abierta", "No cargan los nuevos artículos", "Desde el departamento de ventas vemos que los nuevos artículos que se han introducido en nuestro catálogo desde principios de esta semana no nos aparecen al hacer las consultas del stock general del álmacen"));
+		incidencias.add(new Incidencia("Alta", "SW RRHH", "Aceptada", "Usuario duplicado en Directorio Activo", "Solicitamos que se elimine del directorio activo el usuario user97 ya que se trata de la misma persona que user99 que ha pasado de ser becario a formar parte de la plantilla"));
+		incidencias.add(new Incidencia("Alta", "Microinformatica", "Abierta", "solicitud de nuevo equipo", "Necesitamos un nuevo portátil para el compañero user99 con el software necesario para el departamento de ventas"));
+		incidencias.add(new Incidencia("Alta", "Infraestructura", "Aceptada", "Error Cluster Maquetación", "El servidor 1 del cluster del departamento de maquetación ha caido y al arrancar da un error de pantallazo azul y no termina de levantar"));
+		incidencias.add(new Incidencia("Alta", "Microinformatica", "Abierta", "Posible virus en correo", "El equipo del usuario parece haber estado enviando correos de spam a toda su agenda de contactos. Hemos dejado el equipo apagado, solitamos que se revise por si tuviera virus"));
+		incidencias.add(new Incidencia("Alta", "Correo", "Abierta", "solicitud de aumento de cuota", "Solicito un aumento de la capacidad de mi correo electronico, recibo y envio muchos correos diariamente con adjuntos pesados y trabajar con el archivado local me hace ir mucho más lento."));
+	}
+	
 	@RequestMapping("/login")
 	public String cargaLogin(Model model) {
 
-		//model.addAttribute("name", "World");
-
+		model.addAttribute("incidencias", incidencias);
 		return "login.html";
 	}
 	
@@ -24,7 +40,13 @@ public class GdiControllers {
 		model.addAttribute("pass", password);
 		model.addAttribute("profile", perfil);
 		
-		return "bienvenida_template";
+		if (usuario.equals("usuario"))
+		{
+			return "bienvenida_template";
+		}
+		else {
+			return "bienvenida_tec_template";
+		}
 	}
 	
 	@RequestMapping("/nuevaincidencia")
