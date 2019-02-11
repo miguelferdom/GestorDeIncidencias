f1f8edddb6d435d1ab8ec3685455479325a93273
miguelferdom <miguel.fer.dom@gmail.com>
2019-02-11 05:31:16 +0100

    Modificacion de GdiControllers.java
    Retoco el arrayList para cargar información de incidencias en memoria
    local, añado información para el nuevo campo idIncidencia.
    
    Corrijo la anterior modificación y ahora paso valores de tipo Long en
    vez de int

--- src/main/java/es/urjc/code/gdi/GdiControllers.java
+++ src/main/java/es/urjc/code/gdi/GdiControllers.java
@@ -19,14 +19,14 @@ public class GdiControllers {
 	
 	// declaro el constructor vacio de la clase GdiControllers y lo uso para cargar información de incidencias en memoria
 	public GdiControllers () {
-		incidencias.add(new Incidencia(1, "Alta", "Correo", "Aceptada", "solicitud de nuevo correo electronico", "Necesitamos un buzon de correo para el compañero user99"));
-		incidencias.add(new Incidencia(2, "Alta", "Correo", "Abierta", "Baja de correo electronico", "solicitamos eliminar el buzon de correo del usuario user98 por causar baja en la empresa"));
-		incidencias.add(new Incidencia(3, "Alta", "SW ventas", "Abierta", "No cargan los nuevos artículos", "Desde el departamento de ventas vemos que los nuevos artículos que se han introducido en nuestro catálogo desde principios de esta semana no nos aparecen al hacer las consultas del stock general del álmacen"));
-		incidencias.add(new Incidencia(4, "Alta", "SW RRHH", "Aceptada", "Usuario duplicado en Directorio Activo", "Solicitamos que se elimine del directorio activo el usuario user97 ya que se trata de la misma persona que user99 que ha pasado de ser becario a formar parte de la plantilla"));
-		incidencias.add(new Incidencia(5, "Alta", "Microinformatica", "Abierta", "solicitud de nuevo equipo", "Necesitamos un nuevo portátil para el compañero user99 con el software necesario para el departamento de ventas"));
-		incidencias.add(new Incidencia(6, "Alta", "Infraestructura", "Aceptada", "Error Cluster Maquetación", "El servidor 1 del cluster del departamento de maquetación ha caido y al arrancar da un error de pantallazo azul y no termina de levantar"));
-		incidencias.add(new Incidencia(7, "Alta", "Microinformatica", "Abierta", "Posible virus en correo", "El equipo del usuario parece haber estado enviando correos de spam a toda su agenda de contactos. Hemos dejado el equipo apagado, solitamos que se revise por si tuviera virus"));
-		incidencias.add(new Incidencia(8, "Alta", "Correo", "Abierta", "solicitud de aumento de cuota", "Solicito un aumento de la capacidad de mi correo electronico, recibo y envio muchos correos diariamente con adjuntos pesados y trabajar con el archivado local me hace ir mucho más lento."));
+		incidencias.add(new Incidencia(1L, "Alta", "Correo", "Aceptada", "solicitud de nuevo correo electronico", "Necesitamos un buzon de correo para el compañero user99"));
+		incidencias.add(new Incidencia(2L, "Alta", "Correo", "Abierta", "Baja de correo electronico", "solicitamos eliminar el buzon de correo del usuario user98 por causar baja en la empresa"));
+		incidencias.add(new Incidencia(3L, "Alta", "SW ventas", "Abierta", "No cargan los nuevos artículos", "Desde el departamento de ventas vemos que los nuevos artículos que se han introducido en nuestro catálogo desde principios de esta semana no nos aparecen al hacer las consultas del stock general del álmacen"));
+		incidencias.add(new Incidencia(4L, "Alta", "SW RRHH", "Aceptada", "Usuario duplicado en Directorio Activo", "Solicitamos que se elimine del directorio activo el usuario user97 ya que se trata de la misma persona que user99 que ha pasado de ser becario a formar parte de la plantilla"));
+		incidencias.add(new Incidencia(5L, "Alta", "Microinformatica", "Abierta", "solicitud de nuevo equipo", "Necesitamos un nuevo portátil para el compañero user99 con el software necesario para el departamento de ventas"));
+		incidencias.add(new Incidencia(6L, "Alta", "Infraestructura", "Aceptada", "Error Cluster Maquetación", "El servidor 1 del cluster del departamento de maquetación ha caido y al arrancar da un error de pantallazo azul y no termina de levantar"));
+		incidencias.add(new Incidencia(7L, "Alta", "Microinformatica", "Abierta", "Posible virus en correo", "El equipo del usuario parece haber estado enviando correos de spam a toda su agenda de contactos. Hemos dejado el equipo apagado, solitamos que se revise por si tuviera virus"));
+		incidencias.add(new Incidencia(8L, "Alta", "Correo", "Abierta", "solicitud de aumento de cuota", "Solicito un aumento de la capacidad de mi correo electronico, recibo y envio muchos correos diariamente con adjuntos pesados y trabajar con el archivado local me hace ir mucho más lento."));
 	
 		hincidencias.add("Rojo");
 		hincidencias.add("Verde");
