b6a74e0f297bb5f6b446af25c83d8b576b7b7d67
miguelferdom <miguel.fer.dom@gmail.com>
2019-02-12 12:48:46 +0100

    Modificación de GdiControllers.java
    En el método init añado instrucciones para cargar la información de
    varios incidencias y comentarios ahora conectados por una relacion de
    uno (Incidencia) a muchos (Comentario)
    
    Comento las anteriores pruebas para la carga de datos de incidencias y
    comentarios sin relacion entre ellos

--- src/main/java/es/urjc/code/gdi/GdiControllers.java
+++ src/main/java/es/urjc/code/gdi/GdiControllers.java
@@ -26,8 +26,8 @@ public class GdiControllers {
 	private RepositorioComentario repoComentarios;
 		
 	//creo una lista de incidencias para hacer de la aplicación con la información alojada en memoria
-	private List<Incidencia> incidencias = new ArrayList<>();
-	private List<String> hincidencias = new ArrayList<>();
+	//private List<Incidencia> incidencias = new ArrayList<>();
+	//private List<String> hincidencias = new ArrayList<>();
 	
 	/* declaro el constructor vacio de la clase GdiControllers y lo uso para cargar información de incidencias en memoria
 	public GdiControllers () {
@@ -47,6 +47,7 @@ public class GdiControllers {
 	
 	@PostConstruct
 	public void init() {
+		/*
 		repoIncidencias.save(new Incidencia("Alta", "Correo", "Aceptada", "solicitud de nuevo correo electronico", "Necesitamos un buzon de correo para el compañero user99"));
 		repoIncidencias.save(new Incidencia("Alta", "Correo", "Abierta", "Baja de correo electronico", "solicitamos eliminar el buzon de correo del usuario user98 por causar baja en la empresa"));
 		repoIncidencias.save(new Incidencia("Alta", "SW ventas", "Abierta", "No cargan los nuevos artículos", "Desde el departamento de ventas vemos que los nuevos artículos que se han introducido en nuestro catálogo desde principios de esta semana no nos aparecen al hacer las consultas del stock general del álmacen"));
@@ -55,7 +56,8 @@ public class GdiControllers {
 		repoIncidencias.save(new Incidencia("Alta", "Infraestructura", "Aceptada", "Error Cluster Maquetación", "El servidor 1 del cluster del departamento de maquetación ha caido y al arrancar da un error de pantallazo azul y no termina de levantar"));
 		repoIncidencias.save(new Incidencia("Alta", "Microinformatica", "Abierta", "Posible virus en correo", "El equipo del usuario parece haber estado enviando correos de spam a toda su agenda de contactos. Hemos dejado el equipo apagado, solitamos que se revise por si tuviera virus"));
 		repoIncidencias.save(new Incidencia("Alta", "Correo", "Abierta", "solicitud de aumento de cuota", "Solicito un aumento de la capacidad de mi correo electronico, recibo y envio muchos correos diariamente con adjuntos pesados y trabajar con el archivado local me hace ir mucho más lento."));	
-	
+		*/
+		
 		repoUsuarios.save(new Usuario("user1", "1234", "usuario"));
 		repoUsuarios.save(new Usuario("user2", "1234", "usuario"));
 		repoUsuarios.save(new Usuario("user3", "1234", "usuario"));
@@ -69,6 +71,7 @@ public class GdiControllers {
 		repoUsuarios.save(new Usuario("admi3", "1234", "administrador"));
 		repoUsuarios.save(new Usuario("admi4", "1234", "administrador"));
 
+		/*
 		repoComentarios.save(new Comentario("comentario 01"));
 		repoComentarios.save(new Comentario("comentario 02"));
 		repoComentarios.save(new Comentario("comentario 03"));
@@ -99,6 +102,65 @@ public class GdiControllers {
 		repoComentarios.save(new Comentario("comentario 28"));
 		repoComentarios.save(new Comentario("comentario 29"));
 		repoComentarios.save(new Comentario("comentario 30"));
+		*/
+		
+		Incidencia inc1 = new Incidencia("Alta", "Correo", "Aceptada", "solicitud de nuevo correo electronico", "Necesitamos un buzon de correo para el compañero user99");
+		Incidencia inc2 = new Incidencia("Alta", "Correo", "Abierta", "Baja de correo electronico", "solicitamos eliminar el buzon de correo del usuario user98 por causar baja en la empresa");
+		Incidencia inc3 = new Incidencia("Alta", "SW ventas", "Abierta", "No cargan los nuevos artículos", "Desde el departamento de ventas vemos que los nuevos artículos que se han introducido en nuestro catálogo desde principios de esta semana no nos aparecen al hacer las consultas del stock general del álmacen");
+		Incidencia inc4 = new Incidencia("Alta", "SW RRHH", "Aceptada", "Usuario duplicado en Directorio Activo", "Solicitamos que se elimine del directorio activo el usuario user97 ya que se trata de la misma persona que user99 que ha pasado de ser becario a formar parte de la plantilla");
+		Incidencia inc5 = new Incidencia("Alta", "Microinformatica", "Abierta", "solicitud de nuevo equipo", "Necesitamos un nuevo portátil para el compañero user99 con el software necesario para el departamento de ventas");
+		Incidencia inc6 = new Incidencia("Alta", "Infraestructura", "Aceptada", "Error Cluster Maquetación", "El servidor 1 del cluster del departamento de maquetación ha caido y al arrancar da un error de pantallazo azul y no termina de levantar");
+		Incidencia inc7 = new Incidencia("Alta", "Microinformatica", "Abierta", "Posible virus en correo", "El equipo del usuario parece haber estado enviando correos de spam a toda su agenda de contactos. Hemos dejado el equipo apagado, solitamos que se revise por si tuviera virus");
+		Incidencia inc8 = new Incidencia("Alta", "Correo", "Abierta", "solicitud de aumento de cuota", "Solicito un aumento de la capacidad de mi correo electronico, recibo y envio muchos correos diariamente con adjuntos pesados y trabajar con el archivado local me hace ir mucho más lento.");	
+		
+		inc1.getComentarios().add(new Comentario("comentario 01"));
+		inc1.getComentarios().add(new Comentario("comentario 02"));
+		inc1.getComentarios().add(new Comentario("comentario 03"));
+		inc1.getComentarios().add(new Comentario("comentario 04"));
+		inc1.getComentarios().add(new Comentario("comentario 05"));
+		repoIncidencias.save(inc1);
+		inc2.getComentarios().add(new Comentario("comentario 06"));
+		inc2.getComentarios().add(new Comentario("comentario 07"));
+		inc2.getComentarios().add(new Comentario("comentario 08"));
+		inc2.getComentarios().add(new Comentario("comentario 09"));
+		inc2.getComentarios().add(new Comentario("comentario 10"));
+		repoIncidencias.save(inc2);
+		inc3.getComentarios().add(new Comentario("comentario 11"));
+		inc3.getComentarios().add(new Comentario("comentario 12"));
+		inc3.getComentarios().add(new Comentario("comentario 13"));
+		inc3.getComentarios().add(new Comentario("comentario 14"));
+		inc3.getComentarios().add(new Comentario("comentario 15"));
+		repoIncidencias.save(inc3);
+		inc4.getComentarios().add(new Comentario("comentario 16"));
+		inc4.getComentarios().add(new Comentario("comentario 17"));
+		inc4.getComentarios().add(new Comentario("comentario 18"));
+		inc4.getComentarios().add(new Comentario("comentario 19"));
+		inc4.getComentarios().add(new Comentario("comentario 20"));
+		repoIncidencias.save(inc4);
+		inc5.getComentarios().add(new Comentario("comentario 21"));
+		inc5.getComentarios().add(new Comentario("comentario 22"));
+		inc5.getComentarios().add(new Comentario("comentario 23"));
+		inc5.getComentarios().add(new Comentario("comentario 24"));
+		inc5.getComentarios().add(new Comentario("comentario 25"));
+		repoIncidencias.save(inc5);
+		inc6.getComentarios().add(new Comentario("comentario 26"));
+		inc6.getComentarios().add(new Comentario("comentario 27"));
+		inc6.getComentarios().add(new Comentario("comentario 28"));
+		inc6.getComentarios().add(new Comentario("comentario 29"));
+		inc6.getComentarios().add(new Comentario("comentario 30"));
+		repoIncidencias.save(inc6);
+		inc7.getComentarios().add(new Comentario("comentario 31"));
+		inc7.getComentarios().add(new Comentario("comentario 32"));
+		inc7.getComentarios().add(new Comentario("comentario 33"));
+		inc7.getComentarios().add(new Comentario("comentario 34"));
+		inc7.getComentarios().add(new Comentario("comentario 35"));
+		repoIncidencias.save(inc7);
+		inc8.getComentarios().add(new Comentario("comentario 36"));
+		inc8.getComentarios().add(new Comentario("comentario 37"));
+		inc8.getComentarios().add(new Comentario("comentario 38"));
+		inc8.getComentarios().add(new Comentario("comentario 39"));
+		inc8.getComentarios().add(new Comentario("comentario 40"));
+		repoIncidencias.save(inc8);
 	}
 	
 	@RequestMapping("/login")
