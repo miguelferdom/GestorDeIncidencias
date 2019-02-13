09fe97ed93fd54cfd5648f3af4172351a08a8096
miguelferdom <miguel.fer.dom@gmail.com>
2019-02-13 05:33:23 +0100

    Modificación de Usuario.java
    Añado dos atributos de tipo lista de incidencias (List <Incidencia>)
    para establecer una relación del tipo uno a muchos bidireccional
    (etiqueandolos con @OneToMany(mappedBy="...")) con la clase Incidencia.
    
    Una lista representa las incidencias abiertas por el usuario y la otra
    las atendidas por él.
    
    Implemento los correspondientes metodos get y set para ambos atributos

--- src/main/java/es/urjc/code/gdi/Usuario.java
+++ src/main/java/es/urjc/code/gdi/Usuario.java
@@ -1,9 +1,13 @@
 package es.urjc.code.gdi;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Usuario {
@@ -15,6 +19,13 @@ public class Usuario {
 	private String nombre;
 	private String password;
 	private String perfil;
+	
+	@OneToMany(mappedBy="asignatario")
+	private List <Incidencia> incidenciasAsignadas = new ArrayList<>();
+	
+	@OneToMany(mappedBy="cliente")
+	private List <Incidencia> incidenciasAbiertas = new ArrayList<>();
+		
 	
 	/**
 	 * Constructor de la clase sin parámetros
@@ -59,19 +70,10 @@ public class Usuario {
 	 * 
 	 * @param password Contraseña del usuario en la aplicacion
 	 */
-	public void setPassword(String password) {
+	public void setPassword (String password) {
 		this.password = password;
 	}
 
-	/**
-	 * Método get para devolver el identificador único un Usuario
-	 * 
-	 * @return devuelve el identificador único un Usuario
-	 */
-	public Long getIdUsuario () {
-		return this.idUsuario;
-	}
-	
 	/**
 	 * Método set para establecer el perfil de un Usuario
 	 * 
@@ -82,34 +84,79 @@ public class Usuario {
 	}
 	
 	/**
+	 * Método set para establecer la lista de incidencias atendidas por el usuario
+	 * 
+	 * @param incidenciasAsignadas Lista de incidencias atendidas por el usuario
+	 */
+	public void setIncidenciasAsignadas (List <Incidencia> incidenciasAsignadas) {
+		this.incidenciasAsignadas = incidenciasAsignadas;
+	}
+	
+	/**
+	 * Método set para establecer la lista de incidencias abiertas por el usuario
+	 * 
+	 * @param incidenciasAsignadas Lista de incidencias abiertas por el usuario
+	 */
+	public void setIncidenciasAbiertas (List <Incidencia> incidenciasAbiertas) {
+		this.incidenciasAbiertas = incidenciasAbiertas;
+	}
+	
+	/**
+	 * Método get para devolver el identificador único un Usuario
+	 * 
+	 * @return Devuelve el identificador único un Usuario
+	 */
+	public Long getIdUsuario () {
+		return this.idUsuario;
+	}
+	
+	/**
 	 * Método get para devolver el nombre de un Usuario
 	 * 
-	 * @return devuelve el nombre del usuario
+	 * @return Devuelve el nombre del usuario
 	 */
-	public String getNombre() {
+	public String getNombre () {
 		return this.nombre;
 	}
 	
 	/**
 	 * Método get para devolver el password de un Usuario
 	 * 
-	 * @return devuelve el password del usuario
+	 * @return Devuelve el password del usuario
 	 */
-	public String getPassword() {
+	public String getPassword () {
 		return this.password;
 	}
 	
 	/**
 	 * Método get para devolver el perfil de un Usuario
 	 * 
-	 * @return devuelve el perfil del usuario
+	 * @return Devuelve el perfil del usuario
 	 */
-	public String getPerfil() {
+	public String getPerfil () {
 		return this.perfil;
 	}
 	
+	/**
+	 * Método get para devolver la lista de incidencias atendidas por el usuario
+	 * 
+	 * @return Devuelve la lista de incidencias atendidas por el usuario
+	 */
+	public List <Incidencia> setIncidenciasAsignadas () {
+		return this.incidenciasAsignadas;
+	}
+	
+	/**
+	 * Método get para devolver la lista de incidencias abiertas por el usuario
+	 * 
+	 * @return Devuelve la lista de incidencias abiertas por el usuario
+	 */
+	public List <Incidencia> setIncidenciasAbiertas () {
+		return this.incidenciasAbiertas;
+	}
+	
 	@Override
-	public String toString() {
+	public String toString () {
 		return("[USUARIO] Nombre: " + getNombre() + "; password: " + getPassword() + "; perfil: " + getPerfil());
 	}
 }
