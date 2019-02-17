291626f8a448d2b446d8f686c5ca7772b6f7e000
miguelferdom <miguel.fer.dom@gmail.com>
2019-02-17 13:27:26 +0100

    Creación de la interfaz RepositorioTipoDeProblema.java
    Creo una interfaz que realizará las funciones de repositorio, para poder
    hacer pruebas de persistencia en BBDD para la clase TipoDeProblema.java

---
+++ src/main/java/es/urjc/code/gdi/RepositorioTipoDeProblema.java
@@ -0,0 +1,7 @@
+package es.urjc.code.gdi;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RepositorioTipoDeProblema extends JpaRepository <TipoDeProblema, Long> {
+
+}
