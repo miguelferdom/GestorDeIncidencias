b2430257ad01fe0c3a012c88f0881c69544e0f4b
miguelferdom <miguel.fer.dom@gmail.com>
2019-03-14 05:04:22 +0100

    Creación de UserRepositoryAuthenticationProvider.java
    Añadimos esta nueva clase, tal y como se indica en los apuntes del
    curso, para intentar activar la autenticación de usuarios existentes en
    una base de datos

---
+++ src/main/java/es/urjc/code/gdi/UserRepositoryAuthenticationProvider.java
@@ -0,0 +1,57 @@
+package es.urjc.code.gdi;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserRepositoryAuthenticationProvider implements AuthenticationProvider {
+	
+	@Autowired
+	private RepositorioCrudUsuario repoCrudUsuarios;
+
+	@Override
+	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+		System.out.println("[UserRepositoryAuthenticationProvider] He entrado en el método para autenticar a un usuario!!! ");
+		
+		Usuario user = repoCrudUsuarios.findByNombre(authentication.getName());
+		System.out.println("[UserRepositoryAuthenticationProvider] user nombre: " + user.getNombre());
+		
+		if (user == null) {
+			throw new BadCredentialsException("[UserRepositoryAuthenticationProvider] Usuario no encontrado");
+		}
+
+		String password = (String) authentication.getCredentials();
+		System.out.println("[UserRepositoryAuthenticationProvider] password user: " + user.getPassword());
+		System.out.println("[UserRepositoryAuthenticationProvider] vble password: " + password);
+		
+		if (!new BCryptPasswordEncoder().matches(password, user.getPassword())) {
+			throw new BadCredentialsException("[UserRepositoryAuthenticationProvider] Password erronea!");
+		}
+
+		List<GrantedAuthority> roles = new ArrayList<>();
+
+		for (String role : user.getPerfiles()) {
+			roles.add(new SimpleGrantedAuthority(role));
+		}
+
+		return new UsernamePasswordAuthenticationToken(user.getNombre(), password, roles);
+	}
+
+	@Override
+	public boolean supports(Class<?> authentication) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+}
