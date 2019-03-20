a910eb325da4390764ffba3c30d7a1c382b85d66
miguelferdom <miguel.fer.dom@gmail.com>
2019-03-20 14:44:25 +0100

    Modificación RepositorioIncidencia.java
    añado un par de metodos para buscar las incidencias abiertas por un
    usuario y otro para recuperarlas todas pero ordenadas por su urgencia.

--- src/main/java/es/urjc/code/gdi/RepositorioIncidencia.java
+++ src/main/java/es/urjc/code/gdi/RepositorioIncidencia.java
@@ -1,7 +1,11 @@
 package es.urjc.code.gdi;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RepositorioIncidencia extends JpaRepository <Incidencia, Long> {
+public interface RepositorioIncidencia extends JpaRepository <Incidencia, Long>{
 
+	List <Incidencia> findByCliente(Usuario cliente);
+	List <Incidencia> findAllByOrderByUrgenciaDesc();
 }
