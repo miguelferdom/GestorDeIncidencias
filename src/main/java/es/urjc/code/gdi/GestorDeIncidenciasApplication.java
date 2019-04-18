bdd7ec4b85702d957d41e64c5433d4db591769f5
miguelferdom <miguel.fer.dom@gmail.com>
2019-04-18 20:22:38 +0200

    Mofidicación de GestorDeIncidenciasApplication.java
    Añado la etiqueta @EnableHazelcastHttpSession y el método Config visto
    en los apuntes de la asignatura para implementar sesión distribuida.

--- src/main/java/es/urjc/code/gdi/GestorDeIncidenciasApplication.java
+++ src/main/java/es/urjc/code/gdi/GestorDeIncidenciasApplication.java
@@ -1,13 +1,34 @@
 package es.urjc.code.gdi;
 
+import java.util.Collections;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.session.hazelcast.config.annotation.web.http.EnableHazelcastHttpSession;
+
+import com.hazelcast.config.Config;
+import com.hazelcast.config.JoinConfig;
 
 @SpringBootApplication
+@EnableHazelcastHttpSession
 public class GestorDeIncidenciasApplication {
 
+	@Autowired
+	private RedGdi redgdi;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(GestorDeIncidenciasApplication.class, args);
+	}
+	
+	@Bean
+	public Config config() {
+		Config config = new Config();
+		JoinConfig joinConfig = config.getNetworkConfig().getJoin();
+		joinConfig.getMulticastConfig().setEnabled(false);
+		joinConfig.getTcpIpConfig().setEnabled(true).setMembers(Collections.singletonList(redgdi.getIpRedGdi()));
+		return config;
 	}
 
 }
