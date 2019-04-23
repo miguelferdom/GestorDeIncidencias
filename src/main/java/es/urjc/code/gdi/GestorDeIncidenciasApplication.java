3f7878f93c59e664d7136b1615cedf97e21271e9
miguelferdom <miguel.fer.dom@gmail.com>
2019-04-23 05:59:01 +0200

    Modificación de GestorDeIncidenciasApplication.java
    Pruebas para ver si funciona la sesión de usuario con Hazelcast

--- src/main/java/es/urjc/code/gdi/GestorDeIncidenciasApplication.java
+++ src/main/java/es/urjc/code/gdi/GestorDeIncidenciasApplication.java
@@ -26,7 +26,7 @@ public class GestorDeIncidenciasApplication {
 	public Config config() {
 		Config config = new Config();
 		JoinConfig joinConfig = config.getNetworkConfig().getJoin();
-		joinConfig.getMulticastConfig().setEnabled(true);
+		joinConfig.getMulticastConfig().setEnabled(false);
 		joinConfig.getTcpIpConfig().setEnabled(true).setMembers(Collections.singletonList(redgdi.getIpRedGdi()));
 		return config;
 	}
