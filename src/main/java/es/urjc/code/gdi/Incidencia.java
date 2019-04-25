08d1f5c88e97b583b59943d6ab0844ac142804d1
miguelferdom <miguel.fer.dom@gmail.com>
2019-04-25 16:24:31 +0200

    Modificación de Incidencia.java
    Retoco la relación con la clase Comentario para intentar solucionar un
    error del tipo "failed to lazily initialize a collection of role" que me
    aparece cuando intento consultar la cache de una consulta sobre
    incidencias a la base de datos.

--- src/main/java/es/urjc/code/gdi/Incidencia.java
+++ src/main/java/es/urjc/code/gdi/Incidencia.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -27,7 +28,7 @@ public class Incidencia {
 	private String descripcion;
 	private String solucion;
 	
-	@OneToMany(cascade=CascadeType.ALL)
+	@OneToMany(fetch = FetchType.EAGER, cascade=CascadeType.ALL)
 	private List <Comentario> comentarios = new ArrayList<>();
 	
 	@ManyToOne
