88a44df1c3bcf45941bd207301b9bfe787b804e5
miguelferdom <miguel.fer.dom@gmail.com>
2019-04-29 10:49:04 +0200

    Modificaciones para intentar activar cache de consultas a BD

--- src/main/java/es/urjc/code/gdi/RepositorioDepartamento.java
+++ src/main/java/es/urjc/code/gdi/RepositorioDepartamento.java
@@ -3,7 +3,7 @@ package es.urjc.code.gdi;
 import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-//@CacheConfig(cacheNames="gdi")
+@CacheConfig(cacheNames="gdi")
 public interface RepositorioDepartamento extends JpaRepository <Departamento, Long> {
 
 }
--- src/main/java/es/urjc/code/gdi/RepositorioUsuario.java
+++ src/main/java/es/urjc/code/gdi/RepositorioUsuario.java
@@ -4,9 +4,9 @@ import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-//@CacheConfig(cacheNames="gdi")
+@CacheConfig(cacheNames="gdi")
 public interface RepositorioUsuario extends JpaRepository <Usuario, Long>{
 
-	//@Cacheable
+	@Cacheable
 	Usuario findByNombre(String nombre);
 }
