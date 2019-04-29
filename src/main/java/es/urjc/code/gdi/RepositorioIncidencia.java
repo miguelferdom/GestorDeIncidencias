3c88e449f8e585e1fa917f36835ebfd47338b446
miguelferdom <miguel.fer.dom@gmail.com>
2019-04-29 09:50:25 +0200

    Modificaciones para intentar activar cache de consultas a BD

--- src/main/java/es/urjc/code/gdi/RepositorioIncidencia.java
+++ src/main/java/es/urjc/code/gdi/RepositorioIncidencia.java
@@ -7,15 +7,15 @@ import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-//@CacheConfig(cacheNames="gdi")
+@CacheConfig(cacheNames="gdi")
 public interface RepositorioIncidencia extends JpaRepository <Incidencia, Long>{
 
-	//@CacheEvict(allEntries=true)
+	@CacheEvict(allEntries=true)
 	Incidencia save(Incidencia incidencia);
 	
-	//@Cacheable
+	@Cacheable
 	List <Incidencia> findByCliente(Usuario cliente);
 
-	//@Cacheable
+	@Cacheable
 	List <Incidencia> findAllByOrderByUrgenciaDesc();
 }
